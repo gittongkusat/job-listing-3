00f6fb20158f43c7f3de37f00fad3bc376ba3af1
lsss <610550052@qq.com>
2017-01-14 16:08:28 +0800

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -45,5 +45,5 @@ private
 
   def job_params
 
-  params.require(:job).permit(:title, :description)
+  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
