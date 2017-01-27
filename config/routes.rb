26b2396383aa73121d0574aeee5be18dfe800ec9
lsss <610550052@qq.com>
2017-01-28 01:13:56 +0800

    注册，登入的页面修复完毕

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+    devise_for :users, controllers: { sessions: 'users/sessions' }
 
+    namespace :admin do
+        resources :jobs do
+            member do
+                post :publish
+                post :hide
+            end
 
-
-  namespace :admin do
-    resources :jobs do
-      member do
-        post :publish
-        post :hide
-      end
-
-      resources :resumes
+            resources :resumes
+        end
     end
-  end
 
-  resources :jobs do
-    resources :resumes
-  end
+    resources :jobs do
+        resources :resumes
+    end
 
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'welcome#index'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
